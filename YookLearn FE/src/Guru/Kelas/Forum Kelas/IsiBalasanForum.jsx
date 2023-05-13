f3cd394019128f860aab15d4ea16991bb7e0593b
AldilaRezki <aldilarmdhni@gmail.com>
2023-05-13 14:38:06 +0800

    edit forum dan ubah password

--- YookLearn FE/src/Guru/Kelas/Forum Kelas/IsiBalasanForum.jsx
+++ YookLearn FE/src/Guru/Kelas/Forum Kelas/IsiBalasanForum.jsx
@@ -1,31 +1,45 @@
-import React from "react";
-import { BsPersonFill } from "react-icons/bs";
-import { BsThreeDotsVertical } from "react-icons/bs";
+import React from 'react'
+import { BsFillReplyFill } from 'react-icons/bs';
+import { BsPersonFill } from 'react-icons/bs';
+import { BsThreeDotsVertical } from 'react-icons/bs';
 
 function IsiBalasanForum() {
   return (
-    <div className="flex mt-14 mx-10 pr-10 pl-5 border-[0.3px] shadow-md">
-      <div className="flex flex-col gap-y-5 w-fit py-10">
-        <div className="fotoPembalasForum flex text-3xl mx-auto">
-          <BsPersonFill></BsPersonFill>
+    <div className='flex flex-col mt-14 mx-10 border-[0.3px] shadow-md'>
+        <div className='flex justify-between'>
+            <div className='flex gap-x-12 ml-8'>
+                <div className='flex flex-col gap-y-5 w-fit pt-10'>
+                    <div className='fotoPembalasForum flex text-4xl mx-auto bg-tosca p-5 rounded-full'>
+                        <BsPersonFill></BsPersonFill>
+                    </div>
+                    <span className='namaPembalasForum text-lg mx-auto'>
+                        Lorem Ipsum
+                    </span>
+                </div>
+                <div className='judulDanIsiBalasanForum flex flex-col pt-8 my-auto'>
+                    <span className="judulBalasanForum text-lg">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    </span>
+                    <span className='border-[0.5px] mt-4'></span>
+                    <span className='isiBalasanForum mt-4'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus, massa in gravida fermentum, dolor justo aliquet mauris,
+                    </span>
+                </div>    
+            </div>
+            
+            <div className='flex py-8 mr-10'>
+                <BsThreeDotsVertical></BsThreeDotsVertical>
+            </div>
+        </div>  
+        <div className='flex justify-end mr-10 mb-10'>
+            <div className='flex gap-x-3 text-md text-white bg-biru py-2 px-3 rounded-md'>
+                <BsFillReplyFill className='my-auto'></BsFillReplyFill>
+                <span>Tambahkan balasan</span>
+            </div>
         </div>
-        <span className="namaPembalasForum text-lg mx-auto">Lorem Ipsum</span>
-      </div>
-      <div className="judulDanIsiBalasanForum flex flex-col py-8 ml-16">
-        <span className="judulBalasanForum text-lg">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        </span>
-        <span className="border-[0.5px] mt-4"></span>
-        <span className="isiBalasanForum mt-4">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus,
-          massa in gravida fermentum, dolor justo aliquet mauris,
-        </span>
-      </div>
-      <div className="flex py-8 ml-48">
-        <BsThreeDotsVertical></BsThreeDotsVertical>
-      </div>
     </div>
-  );
+    
+  )
 }
 
-export default IsiBalasanForum;
+export default IsiBalasanForum
--- YookLearn FE/src/Guru/Kelas/Tugas Kelas/TambahTugas.jsx
+++ YookLearn FE/src/Guru/Kelas/Tugas Kelas/TambahTugas.jsx
@@ -33,10 +33,9 @@ function TambahTugas() {
         <div>
             <h2 className='text-md mt-8 ml-10 font-normal text-biru'>Lampiran</h2>
             <div className='flex'>
-                <button className='py-2 px-10 w-fit border-[0.3px] shadow-md ml-10 mt-4'>
-                    Pilih File
-                </button>
-                <span className='ml-5 my-auto '>Tidak ada file yang dipilih</span>    
+                <form className='py-2 w-fit ml-10 mt-4'>
+                    <input type="file" />
+                </form>   
             </div>
             
         </div>
@@ -45,32 +44,18 @@ function TambahTugas() {
             <div>
                 <h2 className='text-md mt-8 font-normal text-biru'>Tenggat</h2>
                 <div className='flex gap-x-8'>
-                    <button className='flex justify-between py-2 pl-5 w-[130px] border-[0.3px] shadow-md mt-4'>
-                        Tanggal
-                        <AiFillCaretDown className='my-auto mr-5'></AiFillCaretDown>
-                    </button>
-                    <button className='flex justify-between py-2 pl-5 w-[130px] border-[0.3px] shadow-md mt-4'>
-                        Bulan
-                        <AiFillCaretDown className='my-auto mr-5'></AiFillCaretDown>
-                    </button>
-                    <button className='flex justify-between py-2 pl-5 w-[130px] border-[0.3px] shadow-md mt-4'>
-                        Tahun
-                        <AiFillCaretDown className='my-auto mr-5'></AiFillCaretDown>
-                    </button>
+                    <form action="">
+                        <input type="date" name="" id="" className='mt-4 py-2 px-5 border-[0.3px] shadow-md'/>
+                    </form>
                 </div>
             </div>
 
             <div>
                 <h2 className='text-md mt-8 font-normal text-biru'>(Opsional)</h2>
                 <div className='flex gap-x-8'>
-                    <button className='flex justify-between py-2 pl-5 w-[130px] border-[0.3px] shadow-md mt-4'>
-                        Jam
-                        <AiFillCaretDown className='my-auto mr-5'></AiFillCaretDown>
-                    </button>
-                    <button className='flex justify-between py-2 pl-5 w-[130px] border-[0.3px] shadow-md mt-4'>
-                        Bulan
-                        <AiFillCaretDown className='my-auto mr-5'></AiFillCaretDown>
-                    </button>
+                    <form action="">
+                        <input type="time" name="" id="" className='mt-4 py-2 px-5 border-[0.3px] shadow-md' />
+                    </form>
                 </div>   
             </div>
 
@@ -87,10 +72,14 @@ function TambahTugas() {
             <div>
                 <h2 className='text-md mt-8 font-normal text-biru'>Setelah melewati tenggat waktu</h2>
                 <div className='flex'>
-                    <button className='flex justify-between py-2 pl-5 w-[360px] border-[0.3px] shadow-md mt-4'>
+                    {/* <button className='flex justify-between py-2 pl-5 w-[360px] border-[0.3px] shadow-md mt-4'>
                         Tidak dapat mengumpulkan tugas
                         <AiFillCaretDown className='my-auto mr-5'></AiFillCaretDown>
-                    </button>
+                    </button> */}
+                    <select className="bg-white outline-none appearance-none focus:border-indigo-600 flex py-2 pl-5 w-[360px] border-[0.3px] shadow-md mt-4">
+                        <option>Tidak dapat mengumpulkan tugas</option>
+                        <option>Dapat mengumpulkan tugas</option>
+                    </select>
                 </div>   
             </div>
         </div>
